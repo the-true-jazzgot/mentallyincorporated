728379a1a66141200b0ffc46ca7faa911187c74f
Era Liwoch <era.liwoch@gmail.com>
2024-10-22 21:11:26 +0200

    Add components files for toasts

---
+++ src/app/components/toast-container/toast-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toast-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './toast-container.component.html',
+  styleUrl: './toast-container.component.scss'
+})
+export class ToastContainerComponent {
+
+}
